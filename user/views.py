6c5b06fd71fde06cf28b3009b8a01f8c0089139a
gary <gary@gmail.com>
2022-02-15 01:32:37 +0800

    2/14

--- user/views.py
+++ user/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, authenticate
+from sympy import Not
 # Create your views here.
+
+
+def user_login(request):
+    if request.method == 'POST':
+        # username = request.POST.get('username')
+        username = request.POST['username']
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)  # request.user
+            # print(user)
+            # return render(request, './case/cases.html')
+            return redirect('cases')
+        else:
+            print('登入失敗')
+
+        # print(username, password)
+
+    return render(request, './user/login.html')
